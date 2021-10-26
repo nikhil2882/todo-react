9670fe9600e6a1f055d4782f0a55eda13f8fd37c
nikhil sengar <nikhil@farmart.co>
2021-10-26 09:15:57 +0530

    todo-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
-function App() {
+export default function App()
+{
+  const [ value, setValue ] = useState("");
+  const [ todos, setTodos ] = useState([]);
+
+  useEffect(function()
+  {
+
+    const request = new XMLHttpRequest();
+
+    request.open("GET", "http://localhost:8000/gettodo");
+    request.send();
+
+    request.addEventListener("load", function()
+    {
+      if(request.status === 200)
+      {
+        setTodos( JSON.parse(request.responseText) );
+      }
+      else
+      {
+        console.log("something went wrong")
+      }
+    })
+
+  }, []);
+        
+  function onChange(event)
+  {
+    setValue(event.target.value);
+  }
+
+  function saveTodos()
+  {
+    const request = new XMLHttpRequest();
+
+    request.open("POST", "http://localhost:8000/save");
+    request.setRequestHeader("Content-type", "application/json");
+    request.send(JSON.stringify({ text: value }));
+
+    request.addEventListener("load", function()
+    {
+      if(request.status === 200)
+      {
+        setTodos([...todos, { text : value}]);
+        setValue("");
+
+      }
+      else
+      {
+        console.log("something went wrong")
+      }
+    })
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input 
+        onChange={onChange} 
+        value={value} 
+        type="text" 
+        placeholder="enter something"
+      />
+      <button onClick={saveTodos}>Submit</button>
+      <ul>
+        {
+          todos.map(function(todo, index)
+          {
+            return <li key={index}>{todo.text}</li>
+          })
+        }
+      </ul>
     </div>
-  );
+  )
 }
-
-export default App;
